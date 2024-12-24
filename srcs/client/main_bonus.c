8a37b3a275a7992b6705ae487c8571627ed5804c
mathisdev7 <999frostz@gmail.com>
2024-12-24 16:30:35 +0100

    fix: fix delay for 42's computers

--- srcs/client/main_bonus.c
+++ srcs/client/main_bonus.c
@@ -6,18 +6,28 @@
 /*   By: mazeghou <mazeghou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 17:08:45 by mazeghou          #+#    #+#             */
-/*   Updated: 2024/11/20 00:45:12 by mazeghou         ###   ########.fr       */
+/*   Updated: 2024/12/24 16:25:45 by mazeghou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/client.h"
 #include "../../printf/ft_printf.h"
 
-void	convert_binary(int server_pid, unsigned char c)
+void	convert_binary(int server_pid, unsigned char c, char *pid_str)
 {
 	int	bit;
+	int	len;
+	int	delay;
 
 	bit = 0;
+	len = ft_strlen(pid_str);
+	delay = 15;
+	if (len > 10000)
+		delay = 35;
+	if (len > 25000)
+		delay = 60;
+	if (len > 50000)
+		delay = 100;
 	while (bit < 8)
 	{
 		if ((c & (0x01 << bit)) != 0)
@@ -26,16 +36,16 @@ void	convert_binary(int server_pid, unsigned char c)
 		}
 		else
 			kill(server_pid, SIGUSR2);
-		usleep(500);
+		usleep(delay);
 		bit++;
 	}
 }
 
-void	send_msg(int pid, char *msg)
+void	send_msg(int pid, char *msg, char *pid_str)
 {
 	while (*msg)
 	{
-		convert_binary(pid, *msg);
+		convert_binary(pid, *msg, pid_str);
 		msg++;
 	}
 }
@@ -53,8 +63,8 @@ int	main(int argc, char **argv)
 			exit(write(1, "\033[31mERROR: PID invalid!\033[0m\n", 30));
 		i++;
 	}
-	send_msg(ft_atoi(argv[1]), argv[2]);
-	send_msg(ft_atoi(argv[1]), "\n");
+	send_msg(ft_atoi(argv[1]), argv[2], argv[1]);
+	send_msg(ft_atoi(argv[1]), "\n", argv[1]);
 	if (argv[1][i] == '\0')
 		ft_printf("\033[32mMessage sent!\033[0m\n");
 	return (0);
